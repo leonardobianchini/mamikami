2a2e37288f9a6f4c87a1006dc80b77cea6da5fdb
leonardoSidney <leonardodasilva1915@gmail.com>
2015-04-07 17:54:05 -0300

    adcionado função que converte cada linha do codigo do usuario para um vetor

--- source/Interpretador.java
+++ source/Interpretador.java
@@ -1,24 +1,54 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
 class Interpretador {
     private int nota;
+    
     public void Token(String a) {
         
     }
+    //função que retorna o numero de linhas que tem o arquivo
     public int getNumeroLinhas(String args) {
         try {
             return this.NumeroLinhas(args);
         } catch (IOException e) {
             System.out.println("Do u try fuck with me?");
         }
-        return -1;
+        return 0;
     }
-    public int NumeroLinhas(String args) throws IOException {
+    //Função que diz exatamente o numero de linhas que tem o arquivo
+    private int NumeroLinhas(String args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(args));
         int linhas = 0;
         while(reader.readLine() != null)
             linhas++;
+        return linhas;
+    }
+    //Função que cria o vetor, chamada pela main
+    public String[] CriaVetor(String args) {
+        try {
+            return this.criaString(args,this.getNumeroLinhas(args));
+        } catch (IOException e) {
+            System.out.println("Arquivo não existente");
+        }
+        return null;
+    }
+    /* 
+    Função que utilizando do numero de linhas e do arquivo, aloca cada linha em um vetor.
+    Tive que adicionar uma exceção a essa merda porque java ficaria me enchendo o saco caso isso nao ocorresse apesar
+    de ter certeza que o arquivo existe porque a função numeroLinhas já diz isso para mim, enfim gg Java.
+    */
+    private String[] criaString(String args, int a) throws IOException {
+        String linhas[] = new String[a];
+        File f = new File(args);
+        Scanner s = new Scanner(f);
+        int i = 0;
+        while(s.hasNext()) {
+            linhas[i] = s.nextLine();
+            i++;
+        }
         return linhas;
     }
 }
--- source/Mamikami.java
+++ source/Mamikami.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 class Mamikami {
 	public static void main(String[] args) {
         Interpretador a = new Interpretador();
-        a.getNumeroLinhas(args[0]);
+        int b = a.getNumeroLinhas(args[0]);
+        String[] linhas = a.CriaVetor(args[0]);
+        for(int i = 0; i < linhas.length ; i++) {
+            System.out.println(linhas[i]);
+        }
     }
 }
