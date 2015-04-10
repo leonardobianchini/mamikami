abb30c083adb3bc8e6798f30152a01995f1e9f86
Harold Becker <Hcsb20@gmail.com>
2015-04-10 11:01:08 -0300

    alterando o nome das funções para algo mais sugestivo

--- source/Interpretador.java
+++ source/Interpretador.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.io.File;
 class Interpretador {
     private int nota;
-    
+
     public String TokenAtribuicaoVariavel(String a) {
         String b = a;
         for(int i = 0; i < a.length() ; i++) {
@@ -17,7 +17,7 @@ class Interpretador {
         return b;
     }
     public String RemovePontoVirgula(String a) {
-        String l = a; 
+        String l = a;
         for(int i = 0; i < a.length(); i++) {
             if(a.charAt(i) == ';') {
                 l = a.substring(0,i);
@@ -44,20 +44,21 @@ class Interpretador {
         return linhas;
     }
     //Função que cria o vetor, chamada pela main
-    public String[] CriaVetor(String args) {
+    public String[] CriaVetorArquivo(String args) {
         try {
-            return this.criaString(args,this.getNumeroLinhas(args));
+            return this.ConverteArquivoVetorString(args,this.getNumeroLinhas(args));
         } catch (IOException e) {
             System.out.println("Arquivo não existente");
         }
         return null;
     }
-    /* 
+    /*
     Função que utilizando do numero de linhas e do arquivo, aloca cada linha em um vetor.
     Tive que adicionar uma exceção a essa merda porque java ficaria me enchendo o saco caso isso nao ocorresse apesar
     de ter certeza que o arquivo existe porque a função numeroLinhas já diz isso para mim, enfim gg Java.
     */
-    private String[] criaString(String args, int a) throws IOException {
+    private String[] ConverteArquivoVetorString(String args, int a) throws IOException {
+        /*é um vetor em que cada indice  tem toda  a a linha transformada em uma String*/
         String linhas[] = new String[a];
         File f = new File(args);
         Scanner s = new Scanner(f);
