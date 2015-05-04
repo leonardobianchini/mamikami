1ea2c4c53a2ee06dd54d53a37bc088a906ea3cb5
Harold Becker <Hcsb20@gmail.com>
2015-05-03 22:01:09 -0300

    variavel refeita

--- source/Variavel.java
+++ source/Variavel.java
@@ -2,6 +2,7 @@ class Variavel{
 
 	private String tipo;
 	private String nome;
+	private Object valor;
 
 //---------------------------------GETES----------------------------------------
 	public String getNome() {
@@ -14,18 +15,18 @@ class Variavel{
 
 	public Object getValor(){
 		if(this.getTipo()== "double"){
-
+			return this.valor;
 		}
 		if(this.getTipo() == "integer"){
-
+			return this.valor;
 		}
 		if(this.getTipo() == "boolean"){
-
+			return this.valor;
 		}
 		if(this.getTipo() == "string"){
-
+			return this.valor;
 		}
-		return null; //tem que retornar algo sabia?
+		return null;
 	}
 
 
@@ -33,8 +34,28 @@ class Variavel{
 	public void setNome(String a) {
 		this.nome = a;
 	}
-	public void setValor(String valoratribuicao){
 
-		
+
+	public void setValor(String valoratribuicao){
+		try{
+			//verifica double
+			this.valor = Double.parseDouble(valoratribuicao);
+			this.tipo = "double";
+		}catch(Exception a){
+			try{
+				this.valor = Integer.parseInt(valoratribuicao);
+				this.tipo = "integer";
+			}catch(Exception a1){
+				try{
+					this.valor = Boolean.parseBoolean(valoratribuicao);
+					this.tipo ="boolean";
+				}catch(Exception a2){
+					this.valor = String.valueOf(valoratribuicao);				
+				}
+			}
+		}
+
+
 	}
 }
+
