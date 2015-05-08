8908d07038a6facb4992bb8017ae115bfec15a56
leonardosidney <leonardodasilva1915@gmail.com>
2015-05-07 22:06:13 -0300

    Removido bug na impressao devido a falta de espaços

--- source/Saida.java
+++ source/Saida.java
@@ -1,41 +1,45 @@
 class Saida {
 	public void imprimir(String a, VetorVariavel v) {
-		this.recursaoPlus(0,a,v);
+		this.imprimindoAsBagacaTudo(a,v);
 	}
 	public void imprimirln(String a, VetorVariavel v) {
-		this.recursaoPlus(0,a,v);
+		this.imprimindoAsBagacaTudo(a,v);
 		System.out.println();
 	}
-	private void recursaoPlus(int i, String a, VetorVariavel v) {
-		while (i < a.length()) {
-			if(a.charAt(i) == ')'){
-				break;
-			}else if (a.charAt(i) == '"') {
-				int j = i+1;
-				while(a.charAt(j) != '"') j++;
-				System.out.print(a.substring(i+1,j));
+	private void imprimindoAsBagacaTudo(String a, VetorVariavel v) {
+		int j, i = 0;
+		//String b;
+		while (a.charAt(i) != ')') {
+			/*
+			if (a.charAt(i) == '(' && a.charAt(i) == '+') {
+				j = i+1;
+				while(a.charAt(j) != ')' && a.charAt(j) != '+') j++;
+				b = a.substring(i+1,j);
+				System.out.println(b.toString());
 				i = j;
-			} else if(a.charAt(i) != ' ' && a.charAt(i) != '+') {
-				int j = i+1;
-				while(j < a.length() && a.charAt(j) != ' ') j++;
-				String b = a.substring(i,j);
-				if (v.EuExisto(b)) {
-					System.out.print(v.getValor(b));
-				} else {
-					try {
-						System.out.print(Double.valueOf(b));
-					} catch (Exception e) {
-						System.out.print(b);
+			}
+			i++; */
+			if(a.charAt(i) == '+' || a.charAt(i) == ')') {
+				if (a.charAt(i-1) == '"') {
+					j = i-2;
+					while(a.charAt(j) != '"') j--;
+					System.out.print(a.substring(j+1,i-1));
+				} else  {
+					j = i-1;
+					while(a.charAt(j) != '(' && a.charAt(j) != '+') j--;
+					String b = a.substring(j+1,i);
+					if (v.EuExisto(b)) {
+						System.out.print(v.getValor(b));
+					} else {
+						try {
+							System.out.print(Double.valueOf(b));
+						} catch (Exception e) {
+							System.out.print(b);
+						}
 					}
 				}
-				i = j;
 			}
 			i++;
-			//if (i == a.length()) break; //Por precaução, vamos deixar comentado...
-			if (i < a.length() && a.charAt(i) == '+') {
-				this.recursaoPlus(i,a,v);
-				break;
-			}
 		}
 	}
 }
