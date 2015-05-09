204a4799f06849335336171d997144aa27c6af59
Harold Becker <Hcsb20@gmail.com>
2015-05-09 15:39:05 -0300

    c,b,m,j, mudança no a

--- source/Uaiou.java
+++ source/Uaiou.java
@@ -2,7 +2,7 @@ class Uaiou extends Condicionais{
 
     public void Loop(String a,String b,VetorVariavel v){
         Token t = new Token();
-        if(getCondicao(this.getValor(a), v))
+        if(getCondicao(a, v))
             t.InterpretaEscopo(a,v);
     }
 }
