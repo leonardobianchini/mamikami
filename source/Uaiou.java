5e286ebdb7dd2904a1eec3694329ec96ff0c8e01
LeonardoSidney <leonardodasilva1915@gmail.com>
2015-05-13 23:28:01 -0300

    modificado visual do while

--- source/Uaiou.java
+++ source/Uaiou.java
@@ -1,8 +1,6 @@
 class Uaiou extends Condicionais {
     public void Loop(String a,String b,VetorVariavel v){
         Token t = new Token();
-        while(getCondicao(a, v)) {
-        	t.InterpretaEscopo(b,v);
-        }
+        while(getCondicao(a, v)) t.InterpretaEscopo(b,v);
     }
 }
